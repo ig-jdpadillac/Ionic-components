0417333c315f069c039f417c81a7b3de957c49a7
Jonathan <jonathan962911@gmail.com>
2019-11-09 19:26:42 -0500

    first commit

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  coponentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirecTo: '/alert'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
